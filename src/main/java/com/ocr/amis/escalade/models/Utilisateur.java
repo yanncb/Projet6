e9c76632e933bc8c8e981e99a2f6dbd467eba819
yanncb <yann.caberas@gmail.com>
2020-02-27 14:30:46 +0100

    suppression classe @Override inutile, plus suppression commentaire.

--- src/main/java/com/ocr/amis/escalade/models/Utilisateur.java
+++ src/main/java/com/ocr/amis/escalade/models/Utilisateur.java
@@ -186,22 +186,22 @@ public class Utilisateur implements Serializable, UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
 
